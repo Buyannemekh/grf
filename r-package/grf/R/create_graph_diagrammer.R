61fb95980a9400ee31b710a3bf10f9087eefbee0
buyannemekh <munkh22b@mtholyoke.edu>
2018-09-13 15:28:24 -0700

    rendering a graph as tree

--- r-package/grf/R/create_graph_diagrammer.R
+++ r-package/grf/R/create_graph_diagrammer.R
@@ -14,4 +14,11 @@ graph <-
                edges_df = edges, 
                directed = TRUE)
 
-render_graph(graph)
+render_graph(graph, layout = "fr")
+
+
+f_graph <- 
+  create_graph() %>% 
+  add_balanced_tree(k=2, h=2) 
+
+render_graph(f_graph, layout = "tree")
